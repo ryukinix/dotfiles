1a1a49fe0d47cbcbcf40e728bb6cae4079ff513d
Manoel Vilela <manoel_vilela@engineer.com>
2017-08-11 06:40:15 -0300

    Update emacs.d on STARFOX by emacsd-push script

--- .emacs.d/personal/hunspell.el
+++ .emacs.d/personal/hunspell.el
@@ -8,31 +8,34 @@
 
 (defvar aspell-name (format "aspell%s" --additional-extension))
 
-;; personal setup for using multiple dictionaries with hunspell
-;; you need: hunspell and hunspell-pt-br
-(with-eval-after-load 'ispell
-  (setq ispell-program-name hunspell-name)
-  (setq ispell-hunspell-dictionary-alist
-        '(("english" "[[:alpha:]]" "[^[:alpha:]]" "[']" t ("-d" "en_US") nil utf-8)
-          ("en_US" "[[:alpha:]]" "[^[:alpha:]]" "[']" t ("-d" "en_US") nil utf-8)
-          ("pt_BR" "[[:alpha:]]" "[^[:alpha:]]" "[']" t ("-d" "pt_BR") nil utf-8)
-          ("brasileiro" "[[:alpha:]]" "[^[:alpha:]]" "[']" t ("-d" "pt_BR") nil utf-8)
-          ("dev" "[[:alpha:]]" "[^[:alpha:]]" "[']" t ("-d" "pt_BR,en_US") nil utf-8)))
-  (setq ispell-dictionary "dev")
-  (setq ispell-dictionary-alist ispell-hunspell-dictionary-alist))
 
-;; only available for hunspell
-(global-set-key [C-f6] (lambda ()
-                         (interactive)
-                         (when (not (equal ispell-program-name hunspell-name))
-                           (setq ispell-program-name hunspell-name))
-                         (ispell-change-dictionary "dev")))
+;; only provide this shortcuts and changes if hunspell is available
+(when (file-exists-p (executable-find hunspell-name))
+  ;; personal setup for using multiple dictionaries with hunspell
+  ;; you need: hunspell and hunspell-pt-br
+  (with-eval-after-load 'ispell
+    (setq ispell-program-name hunspell-name)
+    (setq ispell-hunspell-dictionary-alist
+          '(("english" "[[:alpha:]]" "[^[:alpha:]]" "[']" t ("-d" "en_US") nil utf-8)
+            ("en_US" "[[:alpha:]]" "[^[:alpha:]]" "[']" t ("-d" "en_US") nil utf-8)
+            ("pt_BR" "[[:alpha:]]" "[^[:alpha:]]" "[']" t ("-d" "pt_BR") nil utf-8)
+            ("brasileiro" "[[:alpha:]]" "[^[:alpha:]]" "[']" t ("-d" "pt_BR") nil utf-8)
+            ("dev" "[[:alpha:]]" "[^[:alpha:]]" "[']" t ("-d" "pt_BR,en_US") nil utf-8)))
+    (setq ispell-dictionary "dev")
+    (setq ispell-dictionary-alist ispell-hunspell-dictionary-alist))
 
-;; toggle program for ispell: aspell/hunspell
-(global-set-key [C-f5] (lambda ()
-                         (interactive)
-                         (message "Ispell program set to: %s"
-                                  (if (equal ispell-program-name hunspell-name)
-                                      (progn (ispell-change-dictionary "english")
-                                             (setq ispell-program-name aspell-name))
-                                    (setq ispell-program-name hunspell-name)))))
+  ;; only available for hunspell
+  (global-set-key [C-f6] (lambda ()
+                           (interactive)
+                           (when (not (equal ispell-program-name hunspell-name))
+                             (setq ispell-program-name hunspell-name))
+                           (ispell-change-dictionary "dev")))
+
+  ;; toggle program for ispell: aspell/hunspell
+  (global-set-key [C-f5] (lambda ()
+                           (interactive)
+                           (message "Ispell program set to: %s"
+                                    (if (equal ispell-program-name hunspell-name)
+                                        (progn (ispell-change-dictionary "english")
+                                               (setq ispell-program-name aspell-name))
+                                      (setq ispell-program-name hunspell-name))))))
