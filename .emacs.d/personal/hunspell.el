b04ff7cddeca45097a2a23a2dbdca38ff093368f
Manoel Vilela <manoel_vilela@engineer.com>
2017-08-11 06:29:53 -0300

    Update emacs.d on STARFOX by emacsd-push script

--- .emacs.d/personal/hunspell.el
+++ .emacs.d/personal/hunspell.el
@@ -11,15 +11,15 @@
 ;; personal setup for using multiple dictionaries with hunspell
 ;; you need: hunspell and hunspell-pt-br
 (with-eval-after-load 'ispell
-                (setq ispell-program-name hunspell-name)
-                 (setq ispell-hunspell-dictionary-alist
-                       '(("english" "[[:alpha:]]" "[^[:alpha:]]" "[']" t ("-d" "en_US") nil utf-8)
-                         ("en_US" "[[:alpha:]]" "[^[:alpha:]]" "[']" t ("-d" "en_US") nil utf-8)
-                         ("pt_BR" "[[:alpha:]]" "[^[:alpha:]]" "[']" t ("-d" "pt_BR") nil utf-8)
-                         ("brasileiro" "[[:alpha:]]" "[^[:alpha:]]" "[']" t ("-d" "pt_BR") nil utf-8)
-                         ("dev" "[[:alpha:]]" "[^[:alpha:]]" "[']" t ("-d" "pt_BR,en_US") nil utf-8)))
-                 (ispell-change-dictionary "dev") ;; english and portuguese
-                 (setq ispell-dictionary-alist ispell-hunspell-dictionary-alist))
+  (setq ispell-program-name hunspell-name)
+  (setq ispell-hunspell-dictionary-alist
+        '(("english" "[[:alpha:]]" "[^[:alpha:]]" "[']" t ("-d" "en_US") nil utf-8)
+          ("en_US" "[[:alpha:]]" "[^[:alpha:]]" "[']" t ("-d" "en_US") nil utf-8)
+          ("pt_BR" "[[:alpha:]]" "[^[:alpha:]]" "[']" t ("-d" "pt_BR") nil utf-8)
+          ("brasileiro" "[[:alpha:]]" "[^[:alpha:]]" "[']" t ("-d" "pt_BR") nil utf-8)
+          ("dev" "[[:alpha:]]" "[^[:alpha:]]" "[']" t ("-d" "pt_BR,en_US") nil utf-8)))
+  (setq ispell-dictionary "dev")
+  (setq ispell-dictionary-alist ispell-hunspell-dictionary-alist))
 
 ;; only available for hunspell
 (global-set-key [C-f6] (lambda ()
