5bd3d41cda1d66bb9ba7f0d1138d92d34115c588
Manoel Vilela <manoel_vilela@engineer.com>
2017-10-15 03:03:02 -0300

    [testing] Fix problems on setup.sh: make more resilient
    * Handle errors if the base system don't have git installed.
    * If ssh is installed try use ssh url, if not, install by https.
    * Fix flag --recursively -> --recursive
    * Translate ignore-loop delete and add post-merge-hook.sh too
    * Delete all ignore files only in the end

--- setup.sh
+++ setup.sh
@@ -2,12 +2,28 @@
 
 cd ~/
 
+REPO_NAME=ryukinix/dotfiles
+IGNORED_FILES=(README.md setup.sh install.sh dotdumper.sh post-merge-hook.sh)
+
 if [ -d .dot ]; then
-  echo "Already installed dotfiles"
-  exit 1
+    echo "Already installed dotfiles"
+    exit 1
 fi
 
-git clone --bare git@github.com:ryukinix/dotfiles.git $HOME/.dot --recursively
+if [ ! -f /usr/bin/git]; then
+    echo "Please install git and try again."
+    exit 1
+fi
+
+
+if [ ! -f /usr/bin/ssh]; then
+    DOT_URL=git@github.com:$REPO_NAME.git
+else
+    DOT_URL=https://github.com/$REPO_NAME.git
+fi
+
+
+git clone --bare $DOT_URL $HOME/.dot --recursive
 
 function dot {
   git --git-dir=$HOME/.dot/ --work-tree=$HOME $@
@@ -18,17 +34,17 @@ mkdir -p .dot-backup
 dot checkout
 
 if [ $? = 0 ]; then
-  echo "Checked out dot."
+    echo "Checked out dot."
 else
-  echo "Backing up pre-existing dot files."
+    echo "Backing up pre-existing dot files."
 
-  rm -rf .dot-backup
-  for file in $(dot checkout 2>&1 | egrep "^\s+" | awk {'print $1'})
-  do
-    mkdir -p $(dirname .dot-backup/$file)
-    echo "Backup $file => .dot-backup/$file"
-    mv $file .dot-backup/$file
-  done
+    rm -rf .dot-backup
+    for file in $(dot checkout 2>&1 | egrep "^\s+" | awk {'print $1'})
+    do
+        mkdir -p $(dirname .dot-backup/$file)
+        echo "Backup $file => .dot-backup/$file"
+        mv $file .dot-backup/$file
+    done
 fi
 
 dot reset HEAD . > /dev/null
@@ -37,15 +53,14 @@ dot config status.showUntrackedFiles no
 
 
 
-echo "Dotfiles installed"
+echo "Dotfiles installed."
 
 bash install.sh
 
+# install hook for deleting useless file on git pull
+cp -v ~/post-merge-hook.sh ~/.dot/hooks/post-merge
+
 # ignore loop (remove files which don't belong to dotfiles)
 echo "Removing useless files..."
-for i in README.md setup.sh install.sh dotdumper.sh; do
-    rm -rfv $i
-    dot update-index --assume-unchanged $i
-done
-
-cp ~/post-merge-hook.sh ~/.dot/hooks/post-merge
+rm -rfv ${IGNORE_FILES[@]}
+dot update-index --assume-unchanged ${IGNORE_FILES[@]}
