73f3dab59a2716c78c5db8230a873afcbaca3589
Manoel Vilela <manoel_vilela@engineer.com>
2017-10-15 03:56:21 -0300

    [install.sh] handle errors for install-prelude and install-vim-deps

--- install.sh
+++ install.sh
@@ -10,16 +10,19 @@ fi
 # install prelude for emacs
 function install-prelude {
     cd ~/.emacs.d/
-    git clone --bare https://github.com/bbatsov/prelude.git .git --quiet
-    git config --unset core.bare
-    git reset --hard @
+    if [-d .git]; then
+        git clone --bare https://github.com/bbatsov/prelude.git .git --quiet
+        git config --unset core.bare
+        git reset --hard @ --quiet
+    fi
     cd ~/
 }
 
 # install vim Vundle to handle plugins
 function install-vim-deps {
     mkdir -p ~/.vim/bundle/
-    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim --quiet
+    [[ -d ~/.vim/bundle/Vundle.vim/ ]] || git clone https://github.com/VundleVim/Vundle.vim.git \
+                                                    ~/.vim/bundle/Vundle.vim --quiet
 }
 
 echo "Installing prelude for emacs..."
