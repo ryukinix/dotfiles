adb6bdc349cda4a81b7d73e765453409397456c1
Manoel Vilela <manoel_vilela@engineer.com>
2017-10-15 20:02:43 -0300

    [conf.sh] IGNORED_FILES: rename post-merge-hook.sh to post-hook

--- conf.sh
+++ conf.sh
@@ -4,7 +4,7 @@ IGNORED_FILES=(README.md # these files will be removed
                lib.sh
                conf.sh
                install.sh
-               post-merge-hook.sh)
+               post-hook)
 BACKUP_DIR=.dot-backup/$(date -Ihours)
 PACKAGES=(zsh vim conky emacs tmux)
 DEFAULT_SHELL=zsh
