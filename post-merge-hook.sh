913df4bc76b32d1ddf29152606c477c3b89e2e84
Manoel Vilela <manoel_vilela@engineer.com>
2017-10-15 02:02:44 -0300

    Make post-merge-hook.sh more quiet

--- post-merge-hook.sh
+++ post-merge-hook.sh
@@ -6,9 +6,6 @@
 ignore=(~/install.sh ~/README.md ~/setup.sh ~/dotdumper.sh ~/post-merge-hook.sh)
 
 mkdir -p ~/.dot-backup/
-# backup
-cp -v ${ignore[@]} ~/.dot-backup/
-# remove
-rm -rvf ${ignore[@]}
+mv ${ignore[@]} ~/.dot-backup/ 2> /dev/null || true
 /usr/bin/git --git-dir=$HOME/.dot/ --work-tree=$HOME \
              update-index --assume-unchanged ${ignore[@]}
