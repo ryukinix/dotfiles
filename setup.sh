b60e89b424bba9b68b1c626dea50865ec1ea8fd9
Manoel Vilela <manoel_vilela@engineer.com>
2017-10-15 08:07:14 -0300

    [setup.sh] oh man, go to hell with these errors!!!
    I have a problem here. The mv will throw errors when the base
    directory and it contents are trying to move. For those, just
    silent these errors... will works anyway.
    
    Space on files is just a completely hell.

--- setup.sh
+++ setup.sh
@@ -43,10 +43,10 @@ function backup-dotfiles {
 
     # popule dirs
     conflict-files | xargs dirname | sort | uniq \
-                   | sed "s/^/$BACKUP_DIR\//'" | xargs mkdir -p
+                   | sed "s/^/$BACKUP_DIR\//" | xargs mkdir -p
 
     # mv files
-    conflict-files | xargs mv -t "$BACKUP_DIR"
+    conflict-files | xargs -L 1 -I{in} mv -fv "{in}" "$BACKUP_DIR/{in}" 2> /dev/null # sorry
 }
 
 if [ -d .dot ]; then
