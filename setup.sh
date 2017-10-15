12bee271d4fff7f0ff3b5ef4262227ee8076133b
Manoel Vilela <manoel_vilela@engineer.com>
2017-10-15 06:10:41 -0300

    [setup.sh] refactor setup.sh: split script on small functions

--- setup.sh
+++ setup.sh
@@ -4,10 +4,42 @@ cd ~/
 
 REPO_NAME=ryukinix/dotfiles
 IGNORED_FILES=(README.md setup.sh install.sh dotdumper.sh post-merge-hook.sh)
-BACKUP_DIR=.dot-backup
+BACKUP_DIR=.dot-backup/$(date -Ihours)
 
 function dot {
   git --git-dir=$HOME/.dot/ --work-tree=$HOME $@
+}
+
+function clone-repo {
+    if [ ! -f /usr/bin/git ]; then
+        echo "Please install git and try again."
+        exit 1
+    fi
+
+    if [ -f /usr/bin/ssh ]; then
+        DOT_URL=git@github.com:$REPO_NAME.git
+    else
+        DOT_URL=https://github.com/$REPO_NAME.git
+    fi
+
+    echo "Cloning $DOT_URL."
+    git clone --bare $DOT_URL $HOME/.dot --recursive --quiet
+
+}
+
+
+function backup-dotfiles {
+    echo "Backing up pre-existing dot files on $BACKUP_DIR."
+
+    mkdir -p $BACKUP_DIR
+    # get conflict files (only if really exists)
+    function conflict-files {
+        dot checkout 2>&1 | egrep "^\s+" | awk '{$1=$1;print}' | xargs -d '\n' ls -d | cat
+    }
+    # popule dirs
+    conflict-files | xargs -d '\n' dirname | uniq | xargs -L 1 -d '\n' -I{in} mkdir -p "$BACKUP_DIR/{in}"
+    # mv files
+    conflict-files | xargs -L 1 -d '\n' -I{in} mv "{in}" "$BACKUP_DIR/{in}"
 }
 
 if [ -d .dot ]; then
@@ -15,39 +47,13 @@ if [ -d .dot ]; then
     exit 1
 fi
 
+clone-repo
 
-if [ ! -f /usr/bin/git ]; then
-    echo "Please install git and try again."
-    exit 1
-fi
-
-
-if [ -f /usr/bin/ssh ]; then
-    DOT_URL=git@github.com:$REPO_NAME.git
-else
-    DOT_URL=https://github.com/$REPO_NAME.git
-fi
-
-echo "Cloning $DOT_URL."
-git clone --bare $DOT_URL $HOME/.dot --recursive --quiet
-
-
-mkdir -p .dot-backup
 
 dot checkout &> /dev/null
 
 if [ $? != '0' ]; then
-    echo "Backing up pre-existing dot files on ~/.dot-backup."
-
-    rm -rf $BACKUP_DIR
-    # get conflict files
-    function conflict-files {
-        dot checkout 2>&1 | egrep "^\s+" | awk '{$1=$1;print}'
-    }
-    # popule dirs
-    conflict-files | xargs -d '\n' dirname | uniq | xargs -L 1 -d '\n' -I{in} mkdir -p "$BACKUP_DIR/{in}"
-    # mv files
-    conflict-files | xargs -L 1 -d '\n' -I{in} mv "{in}" "$BACKUP_DIR/{in}"
+    backup-dotfiles
 fi
 
 dot reset HEAD . > /dev/null
@@ -62,7 +68,6 @@ bash install.sh
 cp ~/post-merge-hook.sh ~/.dot/hooks/post-merge
 
 # ignore loop (remove files which don't belong to dotfiles)
-echo "Removing useless files..."
 rm -rf ${IGNORED_FILES[@]}
 dot update-index --assume-unchanged ${IGNORED_FILES[@]}
 
