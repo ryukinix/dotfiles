50a0264398e9552ea7b00a98da4d1fb9e5141ee5
Manoel Vilela <manoel_vilela@engineer.com>
2017-10-16 19:37:26 -0300

    [aliases] add useful aliases: semacs and svim
    Both aliases open sudoedit on the desired editors. Useful
    to edit system files using that editors.

--- .aliases.sh
+++ .aliases.sh
@@ -1,4 +1,6 @@
 alias emacs='emacsclient -nw -a vim'
+alias semacs='SUDO_EDITOR="emacsclient -t -a vim" sudoedit'
+alias svim='SUDO_EDITOR=vim sudoedit'
 alias remacs='sudo rc-service emacs.lerax restart'
 alias agenda='gcalcli agenda'
 alias dotnet-build='msbuild'
