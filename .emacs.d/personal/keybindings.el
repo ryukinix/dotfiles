bece26af536f4b692b6ef0e353390c60e87946e1
Manoel Vilela <manoel_vilela@engineer.com>
2017-10-25 19:04:29 -0300

    [emacs] add alternative company-complete keybinding
    C-f on company-mode-map, because is just intuitive. Forward is
    equal to complete, right?

--- .emacs.d/personal/keybindings.el
+++ .emacs.d/personal/keybindings.el
@@ -99,6 +99,7 @@
 
 ;; company-ode additional keybindings
 (define-key company-mode-map [C-tab] 'company-complete)
+(define-key company-active-map (kbd "C-f") 'company-complete)
 (define-key company-mode-map [C-return] 'company-complete)
 (define-key company-active-map [C-tab] 'company-complete-common-or-cycle)
 (define-key company-active-map [tab] 'company-complete-selection)
