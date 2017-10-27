311bbfd4c1c1dfef7d05aca42fca02b7d7332e48
Manoel Vilela <manoel_vilela@engineer.com>
2017-10-27 16:47:58 -0300

    [emacs] add toggle-window-split and window keybindings
    Added keybindings:
    C-x | -> open window right (horizontally)
    C-x _ -> open window below (vertically)
    C-x / -> toggle layout

--- .emacs.d/personal/keybindings.el
+++ .emacs.d/personal/keybindings.el
@@ -185,3 +185,36 @@
 ;; this avoid weird highlights when I try just copy and paste with a mouse
 ;; selection
 (global-unset-key (kbd "<M-mouse-1>"))
+
+
+
+(defun toggle-window-split ()
+  "Toggle window split layout. Vertical to horizontal and vice-versa.
+   Function stole without shame from https://www.emacswiki.org/emacs/ToggleWindowSplit."
+  (interactive)
+  (if (= (count-windows) 2)
+      (let* ((this-win-buffer (window-buffer))
+         (next-win-buffer (window-buffer (next-window)))
+         (this-win-edges (window-edges (selected-window)))
+         (next-win-edges (window-edges (next-window)))
+         (this-win-2nd (not (and (<= (car this-win-edges)
+                     (car next-win-edges))
+                     (<= (cadr this-win-edges)
+                     (cadr next-win-edges)))))
+         (splitter
+          (if (= (car this-win-edges)
+             (car (window-edges (next-window))))
+          'split-window-horizontally
+        'split-window-vertically)))
+    (delete-other-windows)
+    (let ((first-win (selected-window)))
+      (funcall splitter)
+      (if this-win-2nd (other-window 1))
+      (set-window-buffer (selected-window) this-win-buffer)
+      (set-window-buffer (next-window) next-win-buffer)
+      (select-window first-win)
+      (if this-win-2nd (other-window 1))))))
+
+(global-set-key (kbd "C-x /") 'toggle-window-split)
+(global-set-key (kbd "C-x |") 'split-window-horizontally)
+(global-set-key (kbd "C-x _") 'split-window-vertically)
