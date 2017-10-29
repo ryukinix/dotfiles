d975f4d5fbdb13d21b91e9cf9e3b762a9c618a88
Manoel Vilela <manoel_vilela@engineer.com>
2017-10-29 08:08:23 -0300

    [awesome] add more entries to autoruns.sh

--- .config/awesome/autorun.sh
+++ .config/awesome/autorun.sh
@@ -9,36 +9,39 @@ sysmodmap=/etc/X11/xinit/.Xmodmap
 # merge in defaults and keymaps
 
 if [ -f $sysresources ]; then
-  xrdb -merge $sysresources
+    xrdb -merge $sysresources
 fi
 
 if [ -f $sysmodmap ]; then
-  xmodmap $sysmodmap
+    xmodmap $sysmodmap
 fi
 
 if [ -f "$userresources" ]; then
-  xrdb -merge "$userresources"
+    xrdb -merge "$userresources"
 fi
 
 if [ -f "$usermodmap" ]; then
-  xmodmap "$usermodmap"
+    xmodmap "$usermodmap"
 fi
 
 # MANOEL HELL HERE, be careful or just delete this
 
-# Setup some stuff of xfce4-terminal autostart
-autostart=(~/.config/autostart/Compton.desktop
-           ~/.config/autostart/Zeal.desktop
-           /etc/xdg/autostart/gnome-keyring-pkcs11.desktop
-           /etc/xdg/autostart/gnome-keyring-secrets.desktop
-           /etc/xdg/autostart/gnome-keyring-ssh.desktop
-           /etc/xdg/autostart/xfce-polkit-gnome-authentication-agent-1.desktop
-           /etc/xdg/autostart/at-spi-dbus-bus.desktop
-           /etc/xdg/autostart/pamac-tray.desktop
-           ~/.config/autostart/dropbox.desktop
-           ~/.config/autostart/volumeicon.desktop
-           /usr/share/applications/xfce4-clipman.desktop
-          )
+# Setup some stuff based on xfce4 autostart files
+autostart=(
+    /etc/xdg/autostart/gnome-keyring-pkcs11.desktop
+    /etc/xdg/autostart/gnome-keyring-secrets.desktop
+    /etc/xdg/autostart/gnome-keyring-ssh.desktop
+    /etc/xdg/autostart/xfce-polkit-gnome-authentication-agent-1.desktop
+    /etc/xdg/autostart/at-spi-dbus-bus.desktop
+    /etc/xdg/autostart/pamac-tray.desktop
+    /usr/share/applications/xfce4-clipman.desktop
+    ~/.config/autostart/Compton.desktop
+    ~/.config/autostart/Zeal.desktop
+    ~/.config/autostart/volumeicon.desktop
+    ~/.config/autostart/geary-autostart.desktop
+    ~/.config/autostart/dropbox.desktop
+    ~/.config/autostart/fluxgui.desktop
+)
 
 function notify-dont-exists {
     notify-send "[autoruns.sh] Not found file" `printf "%s" $1`
@@ -65,12 +68,13 @@ function filter-not-running {
 }
 
 function autorun {
+    tasks=`echo ${autostart[@]} | wc -w`
     echo ${autostart[@]} \
         | tr " " "\n" \
         | filter-exists \
         | parse-desktop \
         | filter-not-running \
-        | xargs -L 1 -P 8 -I{in} bash -c "({in} &> /dev/null &) ; exit 0"
+        | xargs -L 1 -P $tasks bash -c "({in} &> /dev/null &) ; exit 0"
 
     (xfdesktop > /dev/null &)&
 }
