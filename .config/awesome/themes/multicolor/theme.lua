a3cddaa4b73f32398de199b511a4897c2e1f1c37
Manoel Vilela <manoel_vilela@engineer.com>
2017-11-01 05:18:43 -0300

    [awesome] increase font size from 8 to 10 on multicolor theme

--- .config/awesome/themes/multicolor/theme.lua
+++ .config/awesome/themes/multicolor/theme.lua
@@ -15,7 +15,7 @@ local os    = { getenv = os.getenv, setlocale = os.setlocale }
 local theme                                     = {}
 theme.confdir                                   = os.getenv("HOME") .. "/.config/awesome/themes/multicolor"
 theme.wallpaper                                 = theme.confdir .. "/wall.png"
-theme.font                                      = "xos4 Terminus 8"
+theme.font                                      = "xos4 Terminus 10"
 theme.menu_bg_normal                            = "#000000"
 theme.menu_bg_focus                             = "#000000"
 theme.bg_normal                                 = "#000000"
