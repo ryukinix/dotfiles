32e3e2fc0a0f6b3b1dd329040e0cd71632f2de1d
Manoel Vilela <manoel_vilela@engineer.com>
2017-11-10 22:38:36 -0300

    Use BACKDIR by seconds instead hours fix #4

--- conf.sh
+++ conf.sh
@@ -6,6 +6,6 @@ IGNORED_FILES=(README.md # these files will be removed
                conf.sh
                install.sh
                post-hook)
-BACKUP_DIR=.dot-backup/$(date -Ihours)
+BACKUP_DIR=.dot-backup/$(date -Iseconds)
 PACKAGES=(zsh vim conky emacs tmux)
 DEFAULT_SHELL=zsh
