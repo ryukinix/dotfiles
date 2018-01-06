16a6eba5a1366b10f9466429566c5ed337cd00b4
Manoel Vilela <manoel_vilela@engineer.com>
2018-01-05 21:01:44 -0300

    [emacs] remove boombayah shit
    This is enough.

--- .emacs.d/personal/keybindings.el
+++ .emacs.d/personal/keybindings.el
@@ -240,11 +240,6 @@
 (setq key-chord-two-keys-delay .015
       key-chord-one-key-delay .020)
 
-(defun boombayah ()
-  "Insert the BOOMBAYAH korean signature"
-  (interactive)
-  (insert "붐바야"))
-
 (dolist (binding
          `((" i" . previous-multiframe-window)
            (" o" . next-multiframe-window)
@@ -276,8 +271,6 @@
 
            (" t" . yas-insert-snippet)
            (" x" . shell)
-           (" b" . boombayah)
-           ;; O Rafael gosta de 붐바야
 
            (" r" . recompile)))
   (key-chord-define-global (car binding) (cdr binding)))
