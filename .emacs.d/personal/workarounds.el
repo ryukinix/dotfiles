ffc635d6768dd37a0d66c0a1d02fbc3ebb453dbe
Manoel Vilela <manoel_vilela@engineer.com>
2018-02-09 03:12:56 -0300

    [emacs] add workarounds for C-c C-l of haskell-mode
    This is related to the whole problem of GHC changes of
    the info at version 8.2.1
    
    This fix was borrowed from:
    https://github.com/haskell/haskell-mode/issues/1553

--- .emacs.d/personal/workarounds.el
+++ .emacs.d/personal/workarounds.el
@@ -6,3 +6,14 @@
     "Trying fix matlab-mode for new versions.
      They are using this old, possibily, deprecated
      variable which at least is not defined on 27.0"))
+
+(with-eval-after-load 'haskell-mode
+  (setq haskell-process-args-ghci
+        '("-ferror-spans" "-fshow-loaded-modules"))
+  (setq haskell-process-args-cabal-repl
+        '("--ghc-options=-ferror-spans -fshow-loaded-modules"))
+  (setq haskell-process-args-stack-ghci
+        '("--ghci-options=-ferror-spans -fshow-loaded-modules"
+          "--no-build" "--no-load"))
+  (setq haskell-process-args-cabal-new-repl
+        '("--ghc-options=-ferror-spans -fshow-loaded-modules")))
