f50de681270b7fc78fbce935d46bedee09e815b0
Manoel Vilela <manoel_vilela@engineer.com>
2018-02-19 00:06:49 -0300

    [emacs] general refactor modes.el: structure definitions
    As always, functions first.

--- .emacs.d/personal/modes.el
+++ .emacs.d/personal/modes.el
@@ -11,16 +11,59 @@
 (require 'company-quickhelp)
 (require 'slime)
 (require 'ssh-agency)
+(require 'ob-latex) ;; require ob-latex for exporting org-latex blocks correctly
 
 
 ;; My modes
 (message "Modos do Manoel")
+
+
+(defun ssh-agency-auth-sock-restore ()
+  "Restore SSH_AUTH_SOCK_BACK and SSH_AUTH_SOCK"
+  (interactive)
+  (let* ((ssh "SSH_AUTH_SOCK")
+         (ssh-back "SSH_AUTH_SOCK_BACK")
+         (current (getenv ssh))
+         (back (getenv ssh-back)))
+    (when back
+      (setenv ssh-back current)
+      (setenv ssh back))
+    (message (getenv ssh))))
+
 
 ;; only activate global-wakatime-mode if the user
 ;; specifically installed this
 (when (package-installed-p 'wakatime-mode)
   ;; wakatime on all files
   (global-wakatime-mode))
+
+;; windows mode section
+(when (eq system-type 'windows-nt)
+  ;; disable gdb because is buggy using from windows
+  ;; use gud-gdb instead
+  (put 'gdb 'disabled t))
+
+;; this avoid crash on daemon-mode at linux when
+;; a frame is closed
+;; more info on:
+;; https://www.gnu.org/software/emacs/manual/html_node/emacs/Clipboard.html
+(when (eq system-type 'gnu/linux)
+  (setq x-select-enable-clipboard-manager nil))
+
+(when (executable-find "xclip")
+  (require 'xclip)
+  (xclip-mode +1))
+
+;; ensure that all the ssh keys was loaded
+(when (executable-find ssh-agency-agent-executable)
+  ;; this will not work if the SSH_AUTH_SOCK was not opened yet
+  ;; On my setup I rely on gnome-keyring to open this shit on XFCE startup
+  ;; so then on a running without X this will not work
+  (let ((hardcoded-auth (getenv "SSH_AUTH_SOCK")))
+    (ssh-agency-ensure)
+    (when hardcoded-auth
+      (setenv "SSH_AUTH_SOCK_BACK" (getenv "SSH_AUTH_SOCK"))
+      (setenv "SSH_AUTH_SOCK" hardcoded-auth))))
 
 ;; move to trash on deleting
 (setq delete-by-moving-to-trash t)
@@ -38,24 +81,8 @@
 (setq org-todo-keywords
       '((sequence "TODO" "IN-PROGRESS" "WAITING" "DONE")))
 
-;; windows mode section
-(when (eq system-type 'windows-nt)
-  ;; disable gdb because is buggy using from windows
-  ;; use gud-gdb instead
-  (put 'gdb 'disabled t))
-
-;; this avoid crash on daemon-mode at linux when
-;; a frame is closed
-;; more info on:
-;; https://www.gnu.org/software/emacs/manual/html_node/emacs/Clipboard.html
-(when (eq system-type 'gnu/linux)
-  (setq x-select-enable-clipboard-manager nil))
-
 ;; add company tooltip align annotations
 (setq company-tooltip-align-annotations t)
-
-;; add company quickhelp mode
-(company-quickhelp-mode +1)
 
 ;; let's go autocomplete FAST
 (setq company-quickhelp-delay 0.1)
@@ -65,9 +92,6 @@
 ;; add slime-company completion tool to slime-contribs
 (push 'slime-company slime-contribs)
 (push 'slime-asdf slime-contribs)
-
-;; this works to use git on command line with core.editor=emacs
-(global-git-commit-mode +1)
 
 ;; setup irc autologin channels
 (setq erc-autojoin-channels-alist '(("freenode.net" "#lispgames")))
@@ -92,35 +116,12 @@
 ;; ░   ░      ░     ░   ▒   ░        ░  ░  ░
 ;; ░  ░       ░         ░  ░░ ░            ░
 ")
-;; ensure that all the ssh keys was loaded
-(when (executable-find ssh-agency-agent-executable)
-  ;; this will not work if the SSH_AUTH_SOCK was not opened yet
-  ;; On my setup I rely on gnome-keyring to open this shit on XFCE startup
-  ;; so then on a running without X this will not work
-  (let ((hardcoded-auth (getenv "SSH_AUTH_SOCK")))
-    (ssh-agency-ensure)
-    (when hardcoded-auth
-        (setenv "SSH_AUTH_SOCK_BACK" (getenv "SSH_AUTH_SOCK"))
-        (setenv "SSH_AUTH_SOCK" hardcoded-auth))))
 
-(defun ssh-agency-auth-sock-restore ()
-  "Restore SSH_AUTH_SOCK_BACK and SSH_AUTH_SOCK"
-  (interactive)
-  (let* ((ssh "SSH_AUTH_SOCK")
-         (ssh-back "SSH_AUTH_SOCK_BACK")
-         (current (getenv ssh))
-         (back (getenv ssh-back)))
-    (when back
-      (setenv ssh-back current)
-      (setenv ssh back))
-    (message (getenv ssh))))
 
 ;; set tab size to 4 (I don't like it 8, very big for me)
 (setq-default tab-width 4)
 
-(when (executable-find "xclip")
-  (require 'xclip)
-  (xclip-mode +1))
-
-;; require ob-latex for exporting org-latex blocks correctly
-(require 'ob-latex)
+;; this works to use git on command line with core.editor=emacs
+(global-git-commit-mode +1)
+;; add company quickhelp mode
+(company-quickhelp-mode +1)
