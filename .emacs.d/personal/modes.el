5015ec55256f71d94bad84ff12ff5394a1ddbea9
Manoel Vilela <manoel_vilela@engineer.com>
2018-02-21 17:55:44 -0300

    [emacs] don't enable ssh-agency on ustar server

--- .emacs.d/personal/modes.el
+++ .emacs.d/personal/modes.el
@@ -55,15 +55,16 @@
   (xclip-mode +1))
 
 ;; ensure that all the ssh keys was loaded
-(when (executable-find ssh-agency-agent-executable)
-  ;; this will not work if the SSH_AUTH_SOCK was not opened yet
-  ;; On my setup I rely on gnome-keyring to open this shit on XFCE startup
-  ;; so then on a running without X this will not work
-  (let ((hardcoded-auth (getenv "SSH_AUTH_SOCK")))
-    (ssh-agency-ensure)
-    (when hardcoded-auth
-      (setenv "SSH_AUTH_SOCK_BACK" (getenv "SSH_AUTH_SOCK"))
-      (setenv "SSH_AUTH_SOCK" hardcoded-auth))))
+(unless (equal system-name "ustar")
+ (when (executable-find ssh-agency-agent-executable)
+   ;; this will not work if the SSH_AUTH_SOCK was not opened yet
+   ;; On my setup I rely on gnome-keyring to open this shit on XFCE startup
+   ;; so then on a running without X this will not work
+   (let ((hardcoded-auth (getenv "SSH_AUTH_SOCK")))
+     (ssh-agency-ensure)
+     (when hardcoded-auth
+       (setenv "SSH_AUTH_SOCK_BACK" (getenv "SSH_AUTH_SOCK"))
+       (setenv "SSH_AUTH_SOCK" hardcoded-auth)))))
 
 ;; move to trash on deleting
 (setq delete-by-moving-to-trash t)
