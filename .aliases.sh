36d92e88797ec16dceaf3117da97a97f2ed01097
Manoel Vilela <manoel_vilela@engineer.com>
2018-02-23 23:54:06 -0300

    [aliases] refactor aliases and add pdf and qemu

--- .aliases.sh
+++ .aliases.sh
@@ -1,3 +1,46 @@
+#!/bin/sh
+
+function u-root-boot {
+    sudo qemu-system-x86_64 \
+         -kernel /boot/vmlinuz-linux-lts \
+         -initrd /tmp/initramfs.linux_amd64.cpio \
+         -append "console=ttyS0" \
+         -nographic \
+         -no-reboot \
+         -net nic,model=rtl8139
+
+}
+
+
+function daemonize {
+    ($1 &> /dev/null&)&
+}
+
+
+# function to run .el files
+function emacs-run {
+    emacsclient -e "(load \"$(pwd)/$1\")"
+}
+
+
+function termbin {
+    link=`nc termbin.com 9999 2> /dev/null | tr -d "\n"`
+    if [ $? -eq '0' ]; then
+        printf $link | xclip -selection clipboard
+        printf "Copied '%s' to X clipboard.\n" $link
+    fi
+}
+
+function dot {
+    GIT_DIR=$HOME/.dot GIT_WORK_TREE=$HOME git $@
+}
+
+# save definition of dot (graphviz language)
+alias dot-graph='/usr/bin/dot'
+alias dot-tig='GIT_DIR=$HOME/.dot/ tig'
+
+
+# ustar is my server
 if [ $(hostname) != "ustar" ]; then
     alias vim=nvim
     alias emacs='emacsclient -nw -a vim'
@@ -7,6 +50,8 @@ else
     alias semacs='SUDO_EDITOR="emacs" sudoedit'
     alias svim='SUDO_EDITOR="vim" sudoedit'
 fi
+
+
 alias remacs='sudo /etc/init.d/emacs.lerax restart'
 alias agenda='gcalcli agenda'
 alias dotnet-build='msbuild'
@@ -16,10 +61,8 @@ alias lain=lein
 alias dic=sdcv
 
 
-
 # don't use nosetests anymore, is legacy broken and fucked
 alias nosetests='nose2'
-
 alias deadstar-bind-up='ssh -Y lerax@deadstar -t "x2x -north -to :0.0"'
 alias deadstar-bind-left='ssh -Y lerax@deadstar -t "x2x -west -to :0.0"'
 alias deadstar-bind-right='ssh -Y lerax@deadstar -t "x2x -east -to :0.0"'
@@ -32,12 +75,7 @@ alias starfox='ssh lerax@starfox'
 alias deadstar='ssh lerax@deadstar'
 alias ustar='ssh server.lerax.me'
 alias lerax.me='ssh server.lerax.me'
-# save definition of dot (graphviz language)
-alias dot-graph='/bin/dot'
-function dot {
-    GIT_DIR=$HOME/.dot GIT_WORK_TREE=$HOME git $@
-}
-alias dot-tig='GIT_DIR=$HOME/.dot/ tig'
+
 
 
 # xclipboard
@@ -45,15 +83,6 @@ alias xcopy='xclip -selection clipboard'
 alias xpaste='xclip -o -selection clipboard'
 
 alias ufc='cd ~/Dropbox/University/Courses/UFC/2017.2/'
-
-
-function termbin {
-    link=`nc termbin.com 9999 2> /dev/null | tr -d "\n"`
-    if [ $? -eq '0' ]; then
-        printf $link | xclip -selection clipboard
-        printf "Copied '%s' to X clipboard.\n" $link
-    fi
-}
 
 # you need tmate to use that
 alias ssh-share='tmux detach -E "tmate && zsh"'
@@ -68,12 +97,6 @@ alias sprungex='sprunge | xcopy'
 alias pastebin=termbin
 
 alias tmate="tmux detach-client -E 'tmate;tmux'"
-
-# function to run .el files
-function emacs-run {
-    emacsclient -e "(load \"$(pwd)/$1\")"
-}
-
 alias stack-size='ulimit -s'
 
 
@@ -86,15 +109,13 @@ alias pacman-remove='sudo pacman -Rsu'
 alias service='sudo rc-service'
 alias pacaur-upgrade='pacaur -Syuua'
 alias pacaur-install='pacaur -Sa'
-alias lelerax='cd ~/Desktop/workspace/dev/lelerax; ./repl'
 
-
-function daemonize {
-    ($1 &> /dev/null&)&
-}
 
 alias aur=trizen
 alias examples=tldr
 alias monitor-off='xset -display :0.0 dpms force off'
-alias monitor-on='xset -display :0.0 dpms force on
-'
+alias monitor-on='xset -display :0.0 dpms force on'
+
+alias pdf=llpp
+alias qemu=qemu-system-x86_64
+alias lelerax='cd ~/Desktop/workspace/dev/lelerax; ./repl'
