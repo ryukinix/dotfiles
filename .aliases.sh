d46f4ec3cc2854d38533321f13bffe194f134f8e
Manoel Vilela <manoel_vilela@engineer.com>
2018-04-03 14:03:03 -0300

    [aliases] add xfdesktop shadow fix

--- .aliases.sh
+++ .aliases.sh
@@ -144,3 +144,4 @@ alias env-down='deactivate'
 alias pyenv-init='eval "$(pyenv init -)"'
 alias pipi='pip install --user'
 alias rot13="tr 'A-Za-z' 'N-ZA-Mn-za-m'"
+alias xfdesktop-shadow-fix='xfconf-query -c xfce4-desktop -p /desktop-icons/center-text -n -t bool -s false'
