24680576e1a8a43ddfd0afffad80007ad5c967b6
Manoel Vilela <manoel_vilela@engineer.com>
2018-10-13 20:48:18 -0300

    [emacs] fix latex.el

--- .emacs.d/personal/latex.el
+++ .emacs.d/personal/latex.el
@@ -3,11 +3,12 @@
 (with-eval-after-load 'ox-latex
   (setq org-latex-listings 'minted
         org-latex-packages-alist '(("" "minted"))
-        org-latex-minted-options '(("frame" "lines") ("linenos" "true"))
+        org-latex-minted-options '(("frame" "lines")
+                                   ("linenos" "true"))
         org-latex-pdf-process
         '("pdflatex -shell-escape -interaction nonstopmode -output-directory %o %f"
           "pdflatex -shell-escape -interaction nonstopmode -output-directory %o %f")
-        org-latex-create-formula-image-program 'imagemagick))
+        org-preview-latex-default-process 'imagemagick))
 
 ;; just increase the preview image
 (plist-put org-format-latex-options :scale 1.2)
