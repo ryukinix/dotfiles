a32fa4e9472c042c4198a68a2e0d3fb1a653042b
Manoel Vilela <manoel_vilela@engineer.com>
2018-12-04 02:09:00 -0300

    [clojure] Remove leipzig-template plugin

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,8 +1,6 @@
 {:repl {:plugins [[cider/cider-nrepl "0.15.1"]]
         :jvm-opts ["-Xms128m" "-Xmx256m"]}
- :user {:plugins [
-                  [leipzig/lein-template "0.4.0-SNAPSHOT"]
-                  [org.clojars.gmoe/overtone-template "1.0.1"]
+ :user {:plugins [[org.clojars.gmoe/overtone-template "1.0.1"]
                   ]}
  ;; unfortunatelly this is not working
  ;; see more on: https://github.com/gmoe/overtone-template/issues/2
