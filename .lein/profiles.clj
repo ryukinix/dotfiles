ee7c166e7b282aa60c541bf421541270f5026bb3
Manoel Vilela <manoel_vilela@engineer.com>
2018-12-04 02:56:28 -0300

    [clojure] Remove overtone-template

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,6 +1,6 @@
-{:repl {:plugins [[cider/cider-nrepl "0.15.1"]]
+{:repl {:plugins [[cider/cider-nrepl "0.18.0"]]
         :jvm-opts ["-Xms128m" "-Xmx256m"]}
- :user {:plugins [[org.clojars.gmoe/overtone-template "1.0.1"]
+ :user {:plugins [ [lein-bin "0.3.4"]
                   ]}
  ;; unfortunatelly this is not working
  ;; see more on: https://github.com/gmoe/overtone-template/issues/2
