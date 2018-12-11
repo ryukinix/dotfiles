5101232a3721bb4e7aae4b9d8d5606b793a38946
Manoel Vilela <manoel_vilela@engineer.com>
2018-12-11 04:11:06 -0300

    [awesome] Adjust layout order

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -86,18 +86,18 @@ local browser      = "exo-open --launch WebBrowser"
 awful.util.terminal = terminal
 awful.util.tagnames = { "1", "2", "3", "4", "5" }
 awful.layout.layouts = {
-   awful.layout.suit.magnifier,
    awful.layout.suit.floating,
+   awful.layout.suit.magnifier,
+   awful.layout.suit.max,
+   awful.layout.suit.max.fullscreen,
    awful.layout.suit.tile,
    awful.layout.suit.tile.left,
    awful.layout.suit.tile.bottom,
    awful.layout.suit.tile.top,
-   awful.layout.suit.fair,
+   --awful.layout.suit.fair,
    --awful.layout.suit.fair.horizontal,
    --awful.layout.suit.spiral,
    --awful.layout.suit.spiral.dwindle,
-   --awful.layout.suit.max,
-   --awful.layout.suit.max.fullscreen,
    --awful.layout.suit.corner.nw,
    --awful.layout.suit.corner.ne,
    --awful.layout.suit.corner.sw,
