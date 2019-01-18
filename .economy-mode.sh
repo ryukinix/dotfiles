a19d899d7ad788e8c54ae6659cb26faca4259f24
Manoel Vilela <manoel_vilela@engineer.com>
2019-01-18 16:17:13 -0300

    [economy] Fix economy-mode

--- .economy-mode.sh
+++ .economy-mode.sh
@@ -17,27 +17,21 @@ function _programid {
     echo $1 | cut -d ' ' -f1
 }
 
-function enable-economy-mode {
-    for serv in $_economy_services; do
-        echo "[info] disabling '$serv' server"
-        sudo rc-service $serv stop
-    done
-    for program in $_economy_programs; do
-        local p=$(_programid "${program}")
-        if pgrep -f $p > /dev/null; then
-            echo "[info] killing '$program' program"
-            pgrep -f $p | xargs kill
-        fi
-    done
-    echo "[info] setting to battery mode with tlp"
-    sudo tlp bat
-}
-
-function disable-economy-mode {
+function economy-run-services {
     for serv in $_economy_services; do
         echo "[info] enabling '$serv' server"
         sudo rc-service $serv start
     done
+}
+
+function economy-kill-services {
+    for serv in $_economy_services; do
+        echo "[info] disabling '$serv' server"
+        sudo rc-service $serv stop
+    done
+}
+
+function economy-run-programs {
     for program in $_economy_programs; do
         local p=$(_programid "${program}")
         if ! pgrep -f $p > /dev/null; then
@@ -45,6 +39,26 @@ function disable-economy-mode {
             daemonize $p > /dev/null
         fi
     done
+}
+
+function economy-kill-programs {
+    for program in $_economy_programs; do
+        local p=$(_programid "${program}")
+        if pgrep -f $p > /dev/null; then
+            echo "[info] killing '$program' program"
+            pgrep -f $p | xargs kill
+        fi
+    done
+}
+
+function economy-mode-enable {
+    economy-kill-services && economy-kill-programs
+    echo "[info] setting to battery mode with tlp"
+    sudo tlp bat
+}
+
+function economy-mode-disable {
+    economy-run-services && economy-run-programs
     echo "[info] setting to AC mode with tlp"
     sudo tlp ac
 }
