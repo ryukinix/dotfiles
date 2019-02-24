3eb8c16ec69ce4bb8f50b79976ec8b7a2f30d367
Manoel Vilela <manoel_vilela@engineer.com>
2019-02-24 09:04:08 -0300

    [emacs] integrate org-ref to latex.el conf

--- .emacs.d/personal/latex.el
+++ .emacs.d/personal/latex.el
@@ -1,12 +1,16 @@
 ;; this enable syntax highlight on org-mode org |> latex |> pdf
+(prelude-require-package 'org-ref)
 (require 'ox-latex)
+(require 'org-ref)
+(require 'org-bibtex)
 (with-eval-after-load 'ox-latex
   (setq org-latex-listings 'minted
         org-latex-packages-alist '(("" "minted"))
         org-latex-minted-options '(("frame" "lines")
                                    ("linenos" "true"))
         org-latex-pdf-process
-        '("pdflatex -shell-escape -interaction nonstopmode -output-directory %o %f"
+        '("latexmk -shell-escape -bibtex -f -pdf %f"
+          "pdflatex -shell-escape -interaction nonstopmode -output-directory %o %f"
           "pdflatex -shell-escape -interaction nonstopmode -output-directory %o %f")
         org-preview-latex-default-process 'imagemagick))
 
@@ -14,7 +18,8 @@
 (plist-put org-format-latex-options :scale 1.2)
 
 (defun clean-export-pdf (&rest _)
-  (call-process-shell-command "rm -rf *.tex _minted*&" nil 0))
+  (call-process-shell-command "rm -rf *.tex *.bbl _minte*&" nil 0)
+  (call-process-shell-command "rm -rf _minte*&" nil 0))
 
 (advice-add 'org-latex-export-to-pdf :after #'clean-export-pdf)
 (advice-add 'org-beamer-export-to-pdf :after #'clean-export-pdf)
