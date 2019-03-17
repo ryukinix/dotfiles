126641cf7c20d06fcc2039fbab168c76d72da1cb
Manoel Vilela <manoel_vilela@engineer.com>
2019-03-17 01:25:22 -0300

    [emacs] Replace vim to lem as alternative editor of emacsclient

--- .aliases.sh
+++ .aliases.sh
@@ -175,7 +175,7 @@ alias dot-tig='GIT_DIR=$HOME/.dot/ tig'
 # ustar is my server
 if [ "$(hostname)" != "ustar" ]; then
     alias vim=nvim
-    alias emacs='emacsclient -nw -a vim'
+    alias emacs='emacsclient -nw -a lem'
     alias semacs='SUDO_EDITOR="emacsclient -t -a vim" sudoedit'
     alias svim='SUDO_EDITOR=vim sudoedit'
 else
