d62f5c2b60de89711ca005917a91f23bf7045d1d
Manoel Vilela <manoel_vilela@engineer.com>
2020-07-18 08:21:04 -0300

    [emacs] Add erc.el with password safe automation

---
+++ .emacs.d/personal/erc.el
@@ -0,0 +1,18 @@
+;; my setup for erc
+
+(require 'erc)
+(require 'subr-x)
+
+(defconst lerax-erc-password-file
+  (expand-file-name "/keybase/private/lerax/secrets/irc.txt.gpg"))
+
+(defun lerax-read-file-password (file)
+  "Read a file password (.gpg) and trim the newlines"
+  (string-trim (with-temp-buffer
+                 (insert-file-contents file)
+                 (buffer-string))))
+
+(defun lerax-erc ()
+  (interactive)
+  (let ((password (lerax-read-file-password lerax-erc-password-file)))
+    (erc :password password)))
