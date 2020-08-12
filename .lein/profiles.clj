2e09ee5023f601ea841066f00df4404e61d84534
Manoel Vilela <manoel_vilela@engineer.com>
2020-08-11 21:41:08 -0300

    [lein] Upgrade cider/cider-nrepl version to 0.25.1

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,4 +1,4 @@
-{:repl {:plugins [[cider/cider-nrepl "0.18.0"]]
+{:repl {:plugins [[cider/cider-nrepl "0.25.1"]]
         :jvm-opts ["-Xms128m" "-Xmx256m"]}
  :user {:plugins [ [lein-bin "0.3.4"]
                   ]}
