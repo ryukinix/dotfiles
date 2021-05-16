e43a115f5b7e604b396a96ea73180394a240d576
Manoel Vilela <manoel.neto@neoway.com.br>
2021-05-16 20:28:24 -0300

    [nyxt] Update config

--- .config/nyxt/init.lisp
+++ .config/nyxt/init.lisp
@@ -1,1 +1,4 @@
-(setf *socket-path* nil)
+(define-configuration browser
+    ((session-restore-prompt :never-restore)
+     (default-modes '(emacs-mode certificate-exception-mode web-mode base-mode))
+     (external-editor-program "/home/lerax/.local/bin/gemacs")))
