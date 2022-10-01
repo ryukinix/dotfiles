bfd3aed3ff7e612e3e43790ec87e76d08a4df2ed
Manoel Vilela <manoel.neto@neoway.com.br>
2022-10-01 14:14:55 -0300

    [offlineimap] Fix get_pass function

--- .offlineimap.py
+++ .offlineimap.py
@@ -3,4 +3,4 @@ from subprocess import check_output
 
 
 def get_pass():
-    return check_output("gpg -dq ~/.offlineimap.gpg", shell=True).strip("\n")
+    return check_output("gpg -dq ~/.offlineimap.gpg", shell=True).strip(b"\n")
