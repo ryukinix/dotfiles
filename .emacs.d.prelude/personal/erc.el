37e20a2373f62a2cfe4785a0995407651b7f0a7f
Manoel Vilela <manoel.neto@neoway.com.br>
2022-10-30 18:09:49 -0300

    [emacs] Update erc module and encrypted credentials

--- .emacs.d.prelude/personal/erc.el
+++ .emacs.d.prelude/personal/erc.el
@@ -1,33 +1,14 @@
-;; (require 'erc)
-;; (require 'erc-log)
-(require 'subr-x)
+(with-eval-after-load 'erc
+  ;; enable notifications -- only if have dbus
+  (add-to-list 'erc-modules 'notifications)
+  (setq erc-default-server "irc.libera.chat")
+  (setq erc-prompt "λ>")
+  (setq erc-save-buffer-on-part nil)
+  (setq erc-save-queries-on-quit nil))
 
-;; ;; == ERC LOG SETUP
-;; (with-eval-after-load 'erc
-;;   ;; enable notifications -- only if have dbus
-;;   (add-to-list 'erc-modules 'notifications)
-;;   (add-to-list 'erc-modules 'log) ;; enable
-;;   (setq erc-default-server "irc.libera.chat")
-;;   (setq erc-prompt "λ>")
-;;   (setq erc-save-buffer-on-part nil)
-;;   (setq erc-save-queries-on-quit nil)
-;;   (when (getenv "EXWM")
-;;     (erc-notify-mode))
-;;   (setq erc-log-write-after-insert t)
-;;   (setq erc-log-write-after-send t)
-;;   (setq erc-log-insert-log-on-open t))
-
-
-(defconst lerax-erc-password-file
-  (expand-file-name "/keybase/private/lerax/secrets/irc.txt.gpg"))
-
-(defun lerax-read-file-password (file)
-  "Read a file password (.gpg) and trim the newlines"
-  (string-trim (with-temp-buffer
-                 (insert-file-contents file)
-                 (buffer-string))))
-
-(defun lerax-erc ()
-  (interactive)
-  (let ((password (lerax-read-file-password lerax-erc-password-file)))
-    (erc :password password)))
+;; == ERC LOG SETUP
+(with-eval-after-load 'erc-log
+  (add-to-list 'erc-modules 'log) ;; enable
+  (setq erc-log-write-after-insert t)
+  (setq erc-log-write-after-send t)
+  (setq erc-log-insert-log-on-open t))
