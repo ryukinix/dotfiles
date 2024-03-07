f856ed070df4e6d66722de805710c4182533beac
Manoel Vilela <manoel.neto@neoway.com.br>
2024-03-06 22:32:27 -0300

    [emacs] Fix leraxy theme in non-daemon session frames

--- .emacs.d.prelude/personal/preload/00-packages.el
+++ .emacs.d.prelude/personal/preload/00-packages.el
@@ -4,43 +4,55 @@
 
 (lerax-init-melpa)
 
-;; NOTE: Sat 20 Jun 2020 05:21:37 PM -03
-;; installing: yasnippet-snippets destroy all my software ↓ fuckups everything, please don't do that
-;; docker-lerax related bug at bootstraping package install
-(lerax-require-packages
- '(
-   auctex
-   company-c-headers
-   company-quickhelp
-   cov
-   darkroom
-   doom-themes
-   flycheck
-   gif-screencast
-   git-commit
-   hl-todo
-   htmlize
-   ix
-   kaolin-themes
-   linum-relative
-   load-env-vars
-   multiple-cursors
-   neotree
-   notmuch
-   org-present
-   org-ref
-   org2jekyll
-   ox-gfm
-   pyvenv
-   restclient
-   simple-modeline
-   slime-company
-   ssh-agency
-   toc-org
-   treemacs
-   visual-fill-column
-   xclip
-   yasnippet
-   zeal-at-point
-   )
- )
+(defun lerax-require-packages-run ()
+  ;; NOTE: Sat 20 Jun 2020 05:21:37 PM -03
+  ;; installing: yasnippet-snippets destroy all my software ↓ fuckups everything, please don't do that
+  ;; docker-lerax related bug at bootstraping package install
+  (lerax-require-packages
+   '(
+     gpt
+     ag ;; not sure why I use this package
+     auctex
+     company-c-headers
+     company-quickhelp
+     cov
+     darkroom
+     doom-themes
+     flycheck
+     gif-screencast
+     git-modes
+     helm-bibtex
+     htmlize
+     ix
+     json-reformat
+     kaolin-themes
+     linum-relative
+     load-env-vars
+     multiple-cursors
+     neotree
+     nlinum
+     notmuch
+     org-present
+     org-ref
+     org2jekyll
+     ox-gfm
+     pkg-info
+     pyvenv
+     restclient
+     simple-modeline
+     slime-company
+     slime-company
+     ssh-agency
+     toc-org
+     treemacs
+     treemacs-projectile
+     visual-fill-column
+     which-key
+     whitespace-cleanup-mode
+     xclip
+     yasnippet
+     zeal-at-point
+     )
+   ))
+
+(lerax-require-packages-run)
