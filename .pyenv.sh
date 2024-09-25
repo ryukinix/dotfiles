482b28de418ea52852594dbe20c31dfbefd2e00c
Manoel Vilela <manoel.neto@neoway.com.br>
2024-09-25 00:11:25 -0300

    [pyenv] Make it sync and --no-rehash to speed up
    async doesn't works

--- .pyenv.sh
+++ .pyenv.sh
@@ -1,10 +1,7 @@
 export PYENV_ROOT="$HOME/.pyenv"
 command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
-async_pyenv_init() {
-    eval "$(pyenv init -)"
-}
-async_pyenv_virtualenv_init() {
+pyenv_init() {
+    eval "$(pyenv init - --path --no-rehash)"
     eval "$(pyenv virtualenv-init -)"
 }
-(async_pyenv_init&)
-(async_pyenv_virtualenv_init&)
+pyenv_init
