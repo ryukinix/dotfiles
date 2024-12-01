e72e718924b2c4d22fd1946fbcbdd6ea29e8f797
Manoel Vilela <manoel.neto@neoway.com.br>
2024-12-01 15:32:04 -0300

    [emacs] update scala-setup and add coursier path to .zprofile

--- .emacs.d.prelude/personal/scala.el
+++ .emacs.d.prelude/personal/scala.el
@@ -1,19 +1,11 @@
 (require 'lerax)
 
 (defun scala-metals ()
-  "You will need emacs-metals in $PATH to work properly.
-
-# Make sure to use coursier v1.1.0-M9 or newer.
-curl -L -o coursier https://git.io/coursier-cli
-chmod +x coursier
-./coursier bootstrap \
-  --java-opt -Xss4m \
-  --java-opt -Xms100m \
-  --java-opt -Dmetals.client=emacs \
-  org.scalameta:metals_2.12:0.9.0 \
-  -r bintray:scalacenter/releases \
-  -r sonatype:snapshots \
-  -o /usr/local/bin/metals-emacs -f
+  "Enable advanced IDE-like features for scala
+   # Get quickly the entire scala dev environment!
+   > curl -fL "https://github.com/coursier/launchers/raw/master/cs-x86_64-pc-linux.gz" | gzip -d > cs
+   > chmod +x cs
+   ./cs setup \
 
 
   Source: https://scalameta.org/metals/docs/editors/emacs.html#installation
@@ -24,18 +16,60 @@ chmod +x coursier
                             lsp-treemacs
                             company-posframe
                             dap-mode))
-  ;; Enable nice rendering of documentation on hover
-  (use-package lsp-ui)
-  ;; Add company-lsp backend for metals
+
   (use-package lsp-mode
     ;; Optional - enable lsp-mode automatically in scala files
+    ;; You could also swap out lsp for lsp-deffered in order to defer loading
     :hook  (scala-mode . lsp)
-    (lsp-mode . lsp-lens-mode))
-  ;; Use the Debug Adapter Protocol for running tests and debugging
-  (use-package dap-mode
-    :hook
-    (lsp-mode . dap-mode)
-    (lsp-mode . dap-ui-mode)))
+    (lsp-mode . lsp-lens-mode)
+    :config
+    ;; Uncomment following section if you would like to tune lsp-mode performance according to
+    ;; https://emacs-lsp.github.io/lsp-mode/page/performance/
+    ;; (setq gc-cons-threshold 100000000) ;; 100mb
+    ;; (setq read-process-output-max (* 1024 1024)) ;; 1mb
+    ;; (setq lsp-idle-delay 0.500)
+    ;; (setq lsp-log-io nil)
+    ;; (setq lsp-completion-provider :capf)
+    (setq lsp-prefer-flymake nil)
+    ;; Makes LSP shutdown the metals server when all buffers in the project are closed.
+    ;; https://emacs-lsp.github.io/lsp-mode/page/settings/mode/#lsp-keep-workspace-alive
+    (setq lsp-keep-workspace-alive nil))
+
+  ;; Add metals backend for lsp-mode
+  (use-package lsp-metals)
+
+  ;; Enable nice rendering of documentation on hover
+  ;;   Warning: on some systems this package can reduce your emacs responsiveness significally.
+  ;;   (See: https://emacs-lsp.github.io/lsp-mode/page/performance/)
+  ;;   In that case you have to not only disable this but also remove from the packages since
+  ;;   lsp-mode can activate it automatically.
+  (use-package lsp-ui)
+
+  ;; Use company-capf as a completion provider.
+  ;;
+  ;; To Company-lsp users:
+  ;;   Company-lsp is no longer maintained and has been removed from MELPA.
+  ;;   Please migrate to company-capf.
+  (use-package company
+    :hook (scala-mode . company-mode)
+    :config
+    (setq lsp-completion-provider :capf))
+
+  (progn ;; dap-mode for debugging
+
+    ;; Posframe is a pop-up tool that must be manually installed for dap-mode
+    (use-package posframe)
+
+    ;; Use the Debug Adapter Protocol for running tests and debugging
+    ;; NOTE(@lerax): dom 01 dez 2024 14:12:48
+    ;; https://github.com/emacs-lsp/dap-mode/issues/796
+    (use-package dap-mode
+      :hook
+      (lsp-mode . dap-mode)
+      (lsp-mode . dap-ui-mode))
+
+    )
+  )
 
 (defun scala-setup ()
   "My scala env for emacs"
@@ -48,7 +82,6 @@ chmod +x coursier
   ;; be careful, it your EATS RAM REALLY FAST!
   ;; 8gb ram do you have? This is not enough!
   (when (and (not (version< emacs-version "26.1"))
-             (executable-find "metals-emacs")
              (>= (lerax-memory-ram) 16))
     (scala-metals))
 
@@ -73,4 +106,4 @@ chmod +x coursier
                                     '(save idle-change new-line mode-enabled))))))
 
 
-;; (scala-setup)
+(scala-setup)
