04a620d503490ae5cbca1015abd5ca6e60ea8d65
Manoel Vilela <manoel_vilela@engineer.com>
2025-01-17 10:58:06 -0300

    [pyenv] fix pyenv init

--- .pyenv.sh
+++ .pyenv.sh
@@ -1,7 +1,6 @@
 export PYENV_ROOT="$HOME/.pyenv"
 command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
 pyenv_init() {
-    eval "$(pyenv init - --path --no-rehash)"
-    eval "$(pyenv virtualenv-init -)"
+    eval "$(pyenv init - -zsh --path --no-rehash)"
 }
 pyenv_init
