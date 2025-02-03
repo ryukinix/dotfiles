b705319ba4dc34eb8f5aa7249fcb2be469f3a26f
Manoel Vilela <manoel.neto@neoway.com.br>
2025-02-03 00:09:55 -0300

    [emacs] refactor gpt module using use-package

--- .emacs.d.prelude/personal/gpt.el
+++ .emacs.d.prelude/personal/gpt.el
@@ -1,10 +1,27 @@
-(require 'gpt)
+(use-package le-gpt
+  :bind (("C-s-g" . le-gpt-chat)
+         ("C-s-n" . le-gpt-complete-at-point)
+         ("C-s-t" . le-gpt-transform-region)
+         ("C-s-s" . le-gpt-select-project-files)
+         ("C-s-d" . le-gpt-deselect-project-files))
+  :config
+  (setq-default le-gpt-openai-key (getenv "OPENAI_KEY"))
+  (setq-default le-gpt-api-type 'openai)
+  (setq-default le-gpt-python-path "/usr/bin/python")
+  :hook
+  (le-gpt-chat-mode . (lambda ()
+                        (interactive)
+                        (whitespace-mode -1))))
+(use-package gpt
+  :bind (("C-M-g" . gpt-chat-no-context)
+         ("C-M-S-g" . gpt-dwi))
 
-(setq-default gpt-openai-key (getenv "OPENAI_KEY"))
-(setq-default gpt-openai-engine "gpt-4o")
-(global-set-key (kbd "C-M-g") 'gpt-dwim-no-context)
-(global-set-key (kbd "C-M-S-g") 'gpt-dwim)
-(add-hook 'gpt-mode-hook (lambda ()
-                          (interactive)
-                          (whitespace-mode -1)
-                          (pyvenv-deactivate)))
+  :config
+  (setq-default gpt-openai-key (getenv "OPENAI_KEY"))
+  (setq-default gpt-model "gpt-4o")
+  (setq-default gpt-python-path "/usr/bin/python")
+
+  :hook
+  (gpt-mode . (lambda ()
+                (interactive)
+                (whitespace-mode -1))))
