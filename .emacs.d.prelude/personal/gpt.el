2a18ddeea95573d09309a95a9ed860f4c0e86a35
Manoel Vilela <manoel.neto@neoway.com.br>
2025-06-05 21:40:45 -0300

    [emacs] use le-gpt as default instead gpt.el due upstream bug
    ref: https://github.com/stuhlmueller/gpt.el/issues/24

--- .emacs.d.prelude/personal/gpt.el
+++ .emacs.d.prelude/personal/gpt.el
@@ -1,5 +1,6 @@
 (use-package le-gpt
   :bind (("C-s-g" . le-gpt-chat)
+         ("C-M-g" . le-gpt-chat)
          ("C-s-n" . le-gpt-complete-at-point)
          ("C-s-t" . le-gpt-transform-region)
          ("C-s-s" . le-gpt-select-project-files)
@@ -12,16 +13,23 @@
   (le-gpt-chat-mode . (lambda ()
                         (interactive)
                         (whitespace-mode -1))))
-(use-package gpt
-  :bind (("C-M-g" . gpt-chat-no-context)
-         ("C-M-S-g" . gpt-chat))
 
-  :config
-  (setq-default gpt-openai-key (getenv "OPENAI_KEY"))
-  (setq-default gpt-model "gpt-4o")
-  (setq-default gpt-python-path "/usr/bin/python")
+;;; NOTE(@lerax): dom 01 jun 2025 12:50:08
+;; after upgrade version 20250525 stopped to work
+;; - gpt-model custom var points to claude even with setq-default pointing to gpt-4o
+;; - gpt-mode major mode is not found when called
+;;
+;; using le-gpt as alternative until got this fixed
 
-  :hook
-  (gpt-mode . (lambda ()
-                (interactive)
-                (whitespace-mode -1))))
+;; (use-package gpt
+;;   :bind (("C-M-g" . gpt-chat-no-context)
+;;          ("C-M-S-g" . gpt-chat))
+;;   :hook
+;;   (gpt-mode . (lambda ()
+;;                 (interactive)
+;;                 (whitespace-mode -1)))
+;;   :config
+;;   (setq gpt-api-type 'openai)
+;;   (setq gpt-openai-key (getenv "OPENAI_KEY"))
+;;   (setq gpt-model "gpt-4o")
+;;   (setq gpt-python-path "/usr/bin/python"))
