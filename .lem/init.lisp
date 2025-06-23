fa402dcc68e9f2ac26ce4db7489b275429ae27e1
Manoel Vilela <manoel.neto@neoway.com.br>
2025-06-23 11:48:28 -0300

    [lem] simplify set font after my pull-request got merged
    https://github.com/lem-project/lem/pull/1825

--- .lem/init.lisp
+++ .lem/init.lisp
@@ -5,37 +5,7 @@
 
 (load-theme "leraxy")
 
-;; FIXME(@lerax): 2025/06/11 15h00
-;; not working properly yet: i can't see the code formatted after calling
-;; M-x format-current-buffer
-;; (defun ruff (buf)
-;;   "Format a Python buffer with ruff."
-;;   (let ((file (buffer-filename buf)))
-;;     (uiop:run-program
-;;      (format nil "rust format ~a" file)
-;;      :ignore-error-status t))
-;;   (revert-buffer t))
-
-;; (lem:register-formatter lem-python-mode:python-mode #'ruff)
-
-#+sdl2
-(defun get-font-by-name-and-kind (name kind)
-  (car (loop for font in (lem-if:get-font-list (lem-core:implementation))
-             when (and (search name font)
-                       (search (format nil "~a.ttf" kind) font))
-                   collect font)))
-
-#+sdl2
-(defun set-font-by-name (font-name)
-  (lem-sdl2/display:change-font 
-   (lem-sdl2/display:current-display)
-   (lem-sdl2/font:make-font-config
-    :latin-normal-file (get-font-by-name-and-kind font-name "Regular")
-    :latin-bold-file (get-font-by-name-and-kind font-name "Bold"))))
-
-#+sdl2
-(set-font-by-name "JuliaMono")
-
+(lem-if:set-font-name "JuliaMono")
 
 (define-command open-init-file () ()
   ;; @sasanidas
@@ -60,9 +30,13 @@
   ("C-h p" 'lem-lisp-mode:lisp-apropos-package)
   ("C-h f" 'lem-lisp-mode:lisp-describe-symbol)
   ("M-C-K" 'kill-current-buffer)
-  ("F8"  'open-init-file)
-  ("M-N" 'lem/line-numbers:toggle-line-numbers)
-  ("F11" 'toggle-frame-fullscreen))
+  ("M-r"   'lem/language-mode:find-references)
+  ("C-x t" 'lem/filer::filer)
+  ("C-?"   'redo)
+  ("C-/"   'undo)
+  ("F8"    'open-init-file)
+  ("M-N"   'lem/line-numbers:toggle-line-numbers)
+  ("F11"   'toggle-frame-fullscreen))
 
 ;; modes
 (lem/auto-save:auto-save-mode)
@@ -70,4 +44,3 @@
 ;; (lem-lisp-mode/paren-coloring:toggle-paren-coloring)
 
 (setq lem-shell-mode:*default-shell-command* "/usr/bin/bash")
-
