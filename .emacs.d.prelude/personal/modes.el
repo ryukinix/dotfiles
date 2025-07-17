f63f3afff548fe3d42fa250832e9273d5c5dcae3
Manoel Vilela <manoel.neto@neoway.com.br>
2025-07-17 12:55:31 -0300

    [emacs] refactor lerax-magit-process-environment
    The previous implementation degrades the speed of magit too much, now
    only using lerax-dotfiles should be work as expected.

--- .emacs.d.prelude/personal/modes.el
+++ .emacs.d.prelude/personal/modes.el
@@ -203,19 +203,12 @@
 
   (defun lerax-magit-process-environment (env)
     "Detect and set git -bare repo env vars when in tracked dotfile directories."
-    (let* ((default (file-name-as-directory (expand-file-name default-directory)))
-           (git-dir (expand-file-name "~/.dot/"))
-           (work-tree (expand-file-name "~/"))
-           (dotfile-dirs
-            (-map (apply-partially 'concat work-tree)
-                  (-uniq (-keep #'file-name-directory
-                                (split-string (shell-command-to-string
-                                               (format "/usr/bin/git --git-dir=%s --work-tree=%s ls-tree --full-tree --name-only -r HEAD"
-                                                       git-dir work-tree))))))))
-      (push work-tree dotfile-dirs)
-      (when (member default dotfile-dirs)
-        (push (format "GIT_WORK_TREE=%s" work-tree) env)
-        (push (format "GIT_DIR=%s" git-dir) env)))
+    (when (equal default-directory (expand-file-name "~/"))
+     (let* ((default (file-name-as-directory (expand-file-name default-directory)))
+            (git-dir (expand-file-name "~/.dot/"))
+            (work-tree (expand-file-name "~/")))
+       (push (format "GIT_WORK_TREE=%s" work-tree) env)
+       (push (format "GIT_DIR=%s" git-dir) env)))
     env)
 
   (advice-add 'magit-process-environment
