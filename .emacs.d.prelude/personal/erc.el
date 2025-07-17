33a94582a2eeff68268551cc1673b703b873d405
Manoel Vilela <manoel.neto@neoway.com.br>
2025-07-16 23:36:09 -0300

    [emacs] update erc module

--- .emacs.d.prelude/personal/erc.el
+++ .emacs.d.prelude/personal/erc.el
@@ -1,15 +1,18 @@
 (with-eval-after-load 'erc
-  ;; enable notifications -- only if have dbus
-  (add-to-list 'erc-modules 'notifications)
+  (require 'erc-join)
+  (require 'erc-log)
+  (add-to-list 'erc-modules 'log)
+  (add-to-list 'erc-modules 'notifications) ;; enable notifications -- only if have dbus
+  (erc-update-modules)
+  (setq erc-log-write-after-insert t)
+  (setq erc-log-write-after-send t)
+  (setq erc-log-insert-log-on-open t)
   (setq erc-default-server "irc.libera.chat")
   (setq erc-prompt "λ>")
   (setq erc-nick "lerax")
   (setq erc-save-buffer-on-part nil)
-  (setq erc-save-queries-on-quit nil))
-
-;; == ERC LOG SETUP
-(with-eval-after-load 'erc-log
-  (add-to-list 'erc-modules 'log) ;; enable
-  (setq erc-log-write-after-insert t)
-  (setq erc-log-write-after-send t)
-  (setq erc-log-insert-log-on-open t))
+  (setq erc-save-queries-on-quit nil)
+  (erc-autojoin-mode +1)
+  (setq erc-autojoin-channels-alist '(("libera.chat" "#emacs" "#lisp" "#linux"))
+        erc-autojoin-timing 'ident
+        erc-autojoin-delay 10))
