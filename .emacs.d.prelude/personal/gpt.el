5cabfcabdacbcfd7435cf464d5d51920df062c5e
Manoel Vilela <manoel.neto@neoway.com.br>
2025-08-09 12:09:16 -0300

    [emacs] use gpt-5

--- .emacs.d.prelude/personal/gpt.el
+++ .emacs.d.prelude/personal/gpt.el
@@ -1,17 +1,16 @@
 (use-package le-gpt
   :bind (("C-s-g" . le-gpt-chat)
-         ("C-M-g" . le-gpt-chat)
          ("C-s-n" . le-gpt-complete-at-point)
          ("C-s-t" . le-gpt-transform-region)
          ("C-s-s" . le-gpt-select-project-files)
          ("C-s-d" . le-gpt-deselect-project-files))
   :config
-  (setq-default le-gpt-openai-key (getenv "OPENAI_KEY"))
-  (setq-default le-gpt-api-type 'openai)
-  (setq-default le-gpt-python-path "/usr/bin/python3")
+  (setq le-gpt-openai-key (getenv "OPENAI_KEY"))
+  (setq le-gpt-model "gpt-4o")
+  (setq le-gpt-api-type 'openai)
+  (setq le-gpt-python-path "/usr/bin/python3")
   :hook
   (le-gpt-chat-mode . (lambda ()
-                        (interactive)
                         (whitespace-mode -1))))
 
 ;;; NOTE(@lerax): dom 01 jun 2025 12:50:08
@@ -28,7 +27,6 @@
 
   :hook
   (gpt-mode . (lambda ()
-                (interactive)
                 (whitespace-mode -1))))
 
 ;;; FIXME(@lerax): dom 15 jun 2025 15:28:24
@@ -43,5 +41,5 @@
   (require 'gpt-mode) ;; otherwise raise error of gpt-mode is not defined!
   (setq gpt-api-type 'openai)
   (setq gpt-openai-key (getenv "OPENAI_KEY"))
-  (setq gpt-model "gpt-4o")
+  (setq gpt-model "gpt-5")
   (setq gpt-python-path "/usr/bin/python3"))
